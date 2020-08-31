1fb8e1c0d24502b559314467c170d0d8ac66496b
DCummin3 <dcummin3@uk.ibm.com>
2020-08-31 10:58:12 +0100

    update test

--- src/content/TrendsPage/__tests__/TrendsPage.test.js
+++ src/content/TrendsPage/__tests__/TrendsPage.test.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { TrendsPage } from '../TrendsPage';
 
-test('renders Trends Page', () => {
-  const { getByText } = render(<TrendsPage />);
-  const searchElement = getByText(/Search Term/i);
-  expect(searchElement).toBeInTheDocument();
-  const selectElement = getByText(/Select/i);
-  expect(selectElement).toBeInTheDocument();
-  const chartTitleElement = getByText(/Trends per Year/i);
-  expect(chartTitleElement).toBeInTheDocument();
+describe('Trends Page', () => {
+  test('renders Trends Page', () => {
+    const { getByText } = render(<TrendsPage />);
+    const searchElement = getByText(/Search Term/i);
+    expect(searchElement).toBeInTheDocument();
+    const selectElement = getByText(/Date Range/i);
+    expect(selectElement).toBeInTheDocument();
+    const chartTitleElement = getByText(/Trends per Year/i);
+    expect(chartTitleElement).toBeInTheDocument();
+  });
 });
